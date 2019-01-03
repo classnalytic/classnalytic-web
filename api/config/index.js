d468810b4c5d386d4ddc3b844db67fdedfcc048b
Wiput Pootong <wiput.pootong@gmail.com>
2019-01-03 17:38:26 +0700

    :fire: remove api

--- api/config/index.js
+++
@@ -1,8 +0,0 @@
-// Choose config key which use depend on NODE_ENV
-if (process.env.NODE_ENV === 'production') {
-  // Production environment
-  module.exports = require('./prod')
-} else {
-  // Development environment
-  module.exports = require('./dev')
-}
--- api/config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  DB_HOST: process.env.DB_HOST,
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
-  JWT_SECRET: process.env.JWT_SECRET
-}
